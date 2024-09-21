7c131c106ea01440407513d191db8192883e3a21
Janajsm <jsmsivam@gmail.com>
2024-09-21 12:11:58 +0530

    first commit

---
+++ face_recognition/webcam.py
@@ -0,0 +1,15 @@
+import cv2
+trainedDataset = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+video=cv2.VideoCapture(0)
+while True:
+    success,frame = video.read()
+    if success == True:
+        gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = trainedDataset.detectMultiScale(gray_img)
+        for x, y, w, h in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.imshow('video', frame)
+        key = cv2.waitKey(1) #One mile second delay
+        if key == 81 or key == 113:
+            break
